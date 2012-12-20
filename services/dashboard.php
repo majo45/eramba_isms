c3a2338f70005a0d67cd567b498b6f44e79f712f
kisero <kisero@gmail.com>
2012-12-20 18:31:35 +0100

    started some work on the dashbards.

--- services/dashboard.php
+++ services/dashboard.php
@@ -6,33 +6,18 @@ include_once("header.php");
 		<div id="widgets-area-wrap">
 			<div id="main-area">
 				<div class="widget">
-					<div class="widget-header">Visits per day</div>
+					<div class="widget-header">Resource Utilization</div>
 					<div class="widget-content">
 						<img src="img/widget-graph.jpg" style="width:100%;"/>
-						<p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, 
-	lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet 
-	nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. </p>
+						<p>This chart represent current (and projected) OPEX, CAPEX and Resource utilization. You can download the data on excel format as well.</p>
+					</div>
+					<div class="widget-header">Service Audit Compliance</div>
+					<div class="widget-content">
+						<img src="img/widget-graph.jpg" style="width:100%;"/>
+						<p>This chart represent current (and projected) OPEX, CAPEX and Resource utilization. You can download the data on excel format as well.</p>
 					</div>
 				</div>
 
-				<div class="widget one-half">
-					<div class="widget-header">One half</div>
-					<div class="widget-content">
-						<img src="img/widget-astronaut.jpg" style="width:100%;"/>
-						<p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, 
-	lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet 
-	nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. </p>
-					</div>
-				</div>
-				<div class="widget second-half">
-					<div class="widget-header">Second half</div>
-					<div class="widget-content">
-						<img src="img/widget-house.jpg" style="width:100%;"/>
-						<p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, 
-	lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet 
-	nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. </p>
-					</div>
-				</div>
 			</div>
 
 		
